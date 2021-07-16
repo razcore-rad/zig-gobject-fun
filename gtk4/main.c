f3761e900bdedf3b49b15d4adf84b42415d5edb0
Logan Rathbone <poprocks@gmail.com>
2021-07-16 15:31:13 -0400

    gtk4: add boilerplate starter program

---
+++ gtk4/main.c
@@ -0,0 +1,33 @@
+// vim: colorcolumn=80 ts=4 sw=4
+
+/* main.c:  boilerplate code to get a gtk4 application up and running. */
+
+#include <gtk/gtk.h>
+
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
+{
+	GtkWidget *window;
+
+	window = gtk_application_window_new (app);
+	gtk_window_set_title (GTK_WINDOW (window), "Window");
+	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+	gtk_widget_show (window);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+	GtkApplication *app;
+	int status;
+
+	app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+	status = g_application_run (G_APPLICATION (app), argc, argv);
+	g_object_unref (app);
+
+	return status;
+}
