221710f1ed1a3b9270234ff2dbe48bc071faeef7
Logan Rathbone <poprocks@gmail.com>
2022-05-26 21:07:20 -0400

    perl-gtk4-demo: Add some nag^H^H^H confirmation dialogs, etc.

--- perl/perl-gtk4-demo-editor/perl-gtk4-demo-editor.pl
+++ perl/perl-gtk4-demo-editor/perl-gtk4-demo-editor.pl
@@ -14,6 +14,7 @@ Glib::Object::Introspection->setup(
 	basename => 'Gio', version => '2.0', package => 'Glib::IO');
 
 our $DATADIR = '.';
+our $app_prettyname;
 
 # work around lack of real bindings for gtk4.
 our $priority_application = 800;
@@ -33,7 +34,54 @@ sub dialog_response_id_to_string
 
 	return $retval;
 }
+
+sub string_to_dialog_response_id
+{
+	my $string = shift;
+	my $retval;
+
+	eval {
+		$retval = Glib::Object::Introspection->convert_sv_to_enum (
+			'Gtk4::ResponseType', $string);
+	};
+	if ($@) {
+		die ("Programmer error; invalid dialog response string:\n\t$@");
+	}
+
+	return $retval;
+}
 # </workarounds>
+
+sub update_titlebar
+{
+	my $widgetsref = shift;
+	my $gfile = $$widgetsref{appwindow}{file};
+
+	$$widgetsref{appwindow}->set_title (sprintf "%s - %s",
+		$gfile ? $gfile->get_basename () : "Untitled",
+		$app_prettyname
+	);
+}
+
+sub create_confirmation_dialog
+{
+	my $parent = shift;
+	my $dialog = Gtk4::Dialog->new ();
+
+	$dialog->set_transient_for ($parent);
+	$dialog->set_modal (1);
+	$dialog->set_title ("Confirm");
+	$dialog->get_content_area->append (Gtk4::Label->new (
+			"The buffer has been modified.\n\n" . 
+			"If you proceed, the contents will be discarded.\n\n" .
+			"Are you sure?")
+	);
+	$dialog->add_button ("_Yes, discard changes", string_to_dialog_response_id ('accept'));
+	$dialog->add_button ("_No, go back", string_to_dialog_response_id ('reject'));
+	$dialog->set_default_response (string_to_dialog_response_id ('reject'));
+
+	return $dialog;
+}
 
 sub dark_mode_action
 {
@@ -66,8 +114,31 @@ sub bold_action
 
 sub quit_action
 {
-	my $app = shift;
-	$app->quit ();
+	my $widgetsref = shift;
+	my $app = $$widgetsref{appwindow}->get_application ();
+	my $buffer = $$widgetsref{textview}->get_buffer ();
+
+	if ($buffer->get_modified ())
+	{
+		my $dialog = create_confirmation_dialog ($$widgetsref{appwindow});
+
+		$dialog->signal_connect ("response" => sub
+			{
+				shift;	# ignore `dialog'; we have it anyway.
+				my $response = dialog_response_id_to_string (shift);
+
+				if ($response eq 'accept') {
+					$app->quit ();
+				}
+				$dialog->destroy ();
+			}	
+		);
+		$dialog->show ();
+	}
+	else
+	{
+		$app->quit ();
+	}
 }
 
 sub about_action
@@ -87,7 +158,33 @@ sub new_action
 	my $widgetsref = shift;
 	my $buffer = $$widgetsref{textview}->get_buffer ();
 
-	$buffer->set_text ("", 0);
+	my $do_new = sub {
+		$buffer->set_text ("", 0);
+		$buffer->set_modified (0);
+		update_titlebar ($widgetsref);
+	};
+
+	if ($buffer->get_modified ())
+	{
+		my $dialog = create_confirmation_dialog ($$widgetsref{appwindow});
+
+		$dialog->signal_connect ("response" => sub
+			{
+				shift;	# ignore `dialog'; we have it anyway.
+				my $response = dialog_response_id_to_string (shift);
+
+				if ($response eq 'accept') {
+					$do_new->();
+				}
+				$dialog->destroy ();
+			}	
+		);
+		$dialog->show ();
+	}
+	else
+	{
+		$do_new->();
+	}
 }
 
 sub open_action
@@ -113,10 +210,70 @@ sub open_action
 				close(FILE);
 
 				$buffer->set_text ($contents, -1);
+				$buffer->set_modified (0);
+				$$widgetsref{appwindow}{file} = $gfile;
+				update_titlebar ($widgetsref);
 			}
 		} 
 	);
-	$chooser->show ();
+
+	if ($buffer->get_modified ())
+	{
+		my $dialog = create_confirmation_dialog ($$widgetsref{appwindow});
+
+		$dialog->signal_connect ("response" => sub
+			{
+				shift;	# ignore `dialog'; we have it anyway.
+				my $response = dialog_response_id_to_string (shift);
+
+				if ($response eq 'accept') {
+					$chooser->show ();
+				}
+				$dialog->destroy ();
+			}	
+		);
+		$dialog->show ();
+	}
+	else
+	{
+		$chooser->show ();
+	}
+}
+
+sub buffer_to_string
+{
+	my $buffer = shift;
+
+	return $buffer->get_text (
+		$buffer->get_start_iter (),
+		$buffer->get_end_iter (),
+		0);
+}
+
+sub write_content_to_gfile
+{
+	my ($content, $gfile) = @_;
+
+	open(FILE, '>', $gfile->get_path ());
+	print FILE $content;
+	close(FILE);
+}
+
+sub save_action
+{
+	my $widgetsref = shift;
+
+	if ($$widgetsref{appwindow}{file})
+	{
+		my $content = buffer_to_string ($$widgetsref{textview}->get_buffer ());
+		write_content_to_gfile ($content, $$widgetsref{appwindow}{file});
+		$$widgetsref{textview}->get_buffer->set_modified (0);
+		update_titlebar ($widgetsref);
+	}
+	else
+	{
+		save_as_action ($widgetsref);
+	}
 }
 
 sub save_as_action
@@ -135,13 +292,12 @@ sub save_as_action
 			if ($response eq 'accept')
 			{
 				my $gfile = $chooser->get_file ();
-				my $content = $buffer->get_text ($buffer->get_start_iter (),
-					$buffer->get_end_iter (),
-					0);
+				my $content = buffer_to_string ($$widgetsref{textview}->get_buffer ());
 
-				open(FILE, '>', $gfile->get_path ());
-				print FILE $content;
-				close(FILE);
+				write_content_to_gfile ($content, $gfile);
+				$$widgetsref{appwindow}{file} = $gfile;
+				$$widgetsref{textview}->get_buffer->set_modified (0);
+				update_titlebar ($widgetsref);
 			}
 		} 
 	);
@@ -185,15 +341,26 @@ sub setup_actions
 
 	# setup app action handlers
 
-	for ("new", "open", "save-as", "quit") {
+	for ("new", "open", "save", "save-as", "quit") {
 		$app_actions{$_} = Glib::IO::SimpleAction->new ($_);
 	}
 
 	$app_actions{new}->signal_connect_swapped (activate => \&new_action, $widgetsref);
 	$app_actions{open}->signal_connect_swapped (activate => \&open_action, $widgetsref);
 	$app_actions{'save-as'}->signal_connect_swapped (activate => \&save_as_action, $widgetsref);
-	$app_actions{quit}->signal_connect_swapped (activate => \&quit_action, $app);
+	$app_actions{quit}->signal_connect_swapped (activate => \&quit_action, $widgetsref);
 
+	# only show save button when document is saveable.
+
+	$app_actions{save}->signal_connect_swapped (activate => \&save_action, $widgetsref);
+	$app_actions{save}->set_enabled (0);
+	$$widgetsref{textview}->get_buffer->signal_connect ('modified-changed' => sub
+		{
+			my $buffer = shift;
+			$app_actions{save}->set_enabled ($buffer->get_modified ());
+		}
+	);
+		
 	# special case: need checkmarks in menu for dark mode
 
 	$app_actions{dark} = Glib::IO::SimpleAction->new_stateful (
@@ -243,6 +410,9 @@ sub activate_cb
 	for ("appwindow", "status", "textview") {
 		$widgets{$_} = $builder->get_object ($_);
 	}
+
+	# cache prettyname from .ui file
+	$app_prettyname = $widgets{appwindow}->get_title ();
 
 	setup_css (\%widgets);
 	setup_settings ($app);
